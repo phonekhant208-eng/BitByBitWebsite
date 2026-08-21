47f6c3d13443db87b5f1457c421c8ecea566ad99
phonekhant208-eng <phonekhant.208@gmail.com>
2026-08-21 23:02:51 +0630

    Update mainpageFunction.js

--- mainpageFunction.js
+++ mainpageFunction.js
@@ -1,5 +1,5 @@
 (function() {
-  const SUPABASE_URL = "https://qyzsymedekmekgosykik.supabase.co";
+  const SUPABASE_URL = "https://mibyte.site";
   const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InF5enN5bWVkZWttZWtnb3N5a2lrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODQzNTM3MTksImV4cCI6MjA5OTkyOTcxOX0.H7cgkvW2gCIX2DiNePoU8hImQI8k6Fo2NK148uC5pPU";
 
   let dbClient;
